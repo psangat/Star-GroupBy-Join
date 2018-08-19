0c36fd47d0f7551fcec858162fdb419ffef917ad
Prajwol Sangat <psangat@hotmail.com>
2018-08-19 19:19:36 +1000

    Project Name Change

--- StarGroupByJoin/StarGroupByJoin/Properties/AssemblyInfo.cs
+++ StarGroupByJoin/StarGroupByJoin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("StarGroupByJoin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("StarGroupByJoin")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c14da42-fced-4690-8968-76bfde88bd03")]
+[assembly: Guid("f9774100-a9b3-4f35-ad74-d5582765cb58")]
 
 // Version information for an assembly consists of the following four values:
 //
